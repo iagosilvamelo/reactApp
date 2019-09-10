8446cb7461f4eb3cf12fbb72f664c65a2bc51bbb
Iago Melo <iagosilvamelo@gmail.com>
2019-09-10 15:32:16 -0300

    Listando produtos da API feita no curso de NodeJS.

--- src/views/main/index.js
+++ src/views/main/index.js
@@ -1,8 +1,39 @@
 import React, { Component } from 'react';
 import api from '../../services/api';
+import './style.css';
 
 export default class Main extends Component {
+    state = {
+        products: []
+    };
+
+    componentDidMount() {
+        this.loadProducts();
+    }
+
+    loadProducts = async () => {
+        const response = await api.get("/Product");
+        this.setState({ products: response.data.docs });
+    };
+
     render() {
-        return <h1>Main page</h1>
+        const { products } = this.state;
+        return (
+            <section>
+                <h1>Main page</h1>
+                <h3>Contagem de produtos: { products.length }</h3>
+
+                <div className="product-list">
+                    {products.map( product => (
+                        <article key={ product._id }>
+                            <strong>{ product.title }</strong>
+                            <p>{ product.description }</p>
+
+                            <a href="#">Acessar</a>
+                        </article>
+                    ))}
+                </div>
+            </section>
+        )
     }
 }
