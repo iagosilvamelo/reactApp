6a57e82b66c12b0d0f3ac080255fad5b6988e5d1
Iago Melo <iagosilvamelo@gmail.com>
2019-09-10 14:49:09 -0300

    Instalado e configurado Axios. Criado view Main.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
+// Bibliotecas
 import React, { Component } from 'react';
+
+// Styles
 import './app.css';
 
-import Header from '../components/template/Header';
+// Views
+import Main from './views/main';
+
+// Components
+import Header from './components/template/Header';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
+        <Main />
       </div>
     );
   }
