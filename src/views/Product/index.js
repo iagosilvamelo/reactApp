48f008ea9ef18af5702a3805542e0fb7f45f5461
Iago Melo <iagosilvamelo@gmail.com>
2019-09-10 18:04:59 -0300

    Criado view de Produto por ID.

--- src/views/Product/index.js
+++ src/views/Product/index.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import api from '../../services/api';
+
+import './style.css';
 
 export default class Product extends Component {
+	state = {
+		product: [],
+	};
+
+	async componentDidMount() {
+		const { id } = this.props.match.params;
+		const response = await api.get(`/Product/${id}`);
+		this.setState({ product: response.data });
+	}
+
     render() {
+    	const { product } = this.state;
         return (
-            <section>
-                <h1>Product</h1>
+            <section className="product-info">
+	                <h1>{ product.title }</h1>
+	                <p>{ product.description }</p>
+
+	                <p>
+	                	URL: <a href={ product.url }>{ product.url }</a>
+	                </p>
+
+	               <Link to="/Produtos" className="btn">Voltar</Link>
             </section>
         )
     }
